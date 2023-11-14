3bcd4449ba4f1097f046312729b035d7d5ac426c
Samer luben mahamid <119699134+Samerluben@users.noreply.github.com>
2023-11-14 22:56:58 +0100

    change new

---
+++ src/new.py
@@ -0,0 +1,38 @@
+Last login: Tue Nov 14 21:23:05 on console
+samerlubenmahamid@Samers-MacBook-Air ~ % git clone https://github.com/samerlu99/Informatik-pro-.git
+fatal: destination path 'Informatik-pro-' already exists and is not an empty directory.
+samerlubenmahamid@Samers-MacBook-Air ~ % cd Inforamtik-pro-
+cd: no such file or directory: Inforamtik-pro-
+samerlubenmahamid@Samers-MacBook-Air ~ % cd Informatik-pro-
+samerlubenmahamid@Samers-MacBook-Air Informatik-pro- % ls
+README.md	aufgabe 7	blatt2		blatt4		src
+aufgabe		blatt1		blatt3		blatt5
+samerlubenmahamid@Samers-MacBook-Air Informatik-pro- % cd blatt1
+cd: not a directory: blatt1
+samerlubenmahamid@Samers-MacBook-Air Informatik-pro- % cd blatt5
+cd: not a directory: blatt5
+samerlubenmahamid@Samers-MacBook-Air Informatik-pro- % git init
+Reinitialized existing Git repository in /Users/samerlubenmahamid/Informatik-pro-/.git/
+samerlubenmahamid@Samers-MacBook-Air Informatik-pro- % ls
+README.md	aufgabe 7	blatt2		blatt4		src
+aufgabe		blatt1		blatt3		blatt5
+samerlubenmahamid@Samers-MacBook-Air Informatik-pro- % cd aufgabe 7
+cd: string not in pwd: aufgabe
+samerlubenmahamid@Samers-MacBook-Air Informatik-pro- % cd src
+samerlubenmahamid@Samers-MacBook-Air src % ls
+Main.java	README.md	second2.java
+samerlubenmahamid@Samers-MacBook-Air src % git init
+hint: Using 'master' as the name for the initial branch. This default branch name
+hint: is subject to change. To configure the initial branch name to use in all
+hint: of your new repositories, which will suppress this warning, call:
+hint:
+hint: 	git config --global init.defaultBranch <name>
+hint:
+hint: Names commonly chosen instead of 'master' are 'main', 'trunk' and
+hint: 'development'. The just-created branch can be renamed via this command:
+hint:
+hint: 	git branch -m <name>
+Initialized empty Git repository in /Users/samerlubenmahamid/Informatik-pro-/src/.git/
+samerlubenmahamid@Samers-MacBook-Air src %
+
+
